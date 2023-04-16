1c60fc0c35a977f3d3edcd52ec1ad1c39e5c8623
tejasviii <102219382+tejasviii@users.noreply.github.com>
2023-04-17 00:15:41 +0530

    Create MailConstants.java

---
+++ src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,11 @@
+package org.geekster;
+public class MailConstants {
+
+    public static final String SENDER = "chauhantejasvi479@gmail.com";
+
+    public static final String RECEIVERMAILADDRESS = "chauhanarpana01@gmail.com";
+
+    public static final String SUBJECT = "Testing Java mail............";
+
+    public static final String MESSAGE = "Hello how are you...???";
+}
