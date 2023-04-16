e4a57426a71eb5dbb9f726ab48e16517b7a82ac1
tejasviii <102219382+tejasviii@users.noreply.github.com>
2023-04-17 00:16:42 +0530

    Create Main.java

---
+++ src/main/java/org/geekster/Main.java
@@ -0,0 +1,14 @@
+package org.geekster;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("This application is supposed to send mails using java😁😁");
+
+        HandleMail mailer = new HandleMail();
+
+        mailer.sendMail();
+
+    }
+}
